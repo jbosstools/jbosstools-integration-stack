102828de1e298dc9aae20c13a737e5f8f72a872a
Paul Leacu <pleacu@redhat.com>
2016-05-18 16:39:16 -0400

    JBTIS-607 - correct checkstyle issues

--- devstudio/tests/com.jboss.jbds.integration-stack.tests.test/src/com/jboss/jbds/integration_stack/tests/test/IDEPropertiesConstants.java
+++ devstudio/tests/com.jboss.jbds.integration-stack.tests.test/src/com/jboss/jbds/integration_stack/tests/test/IDEPropertiesConstants.java
@@ -14,29 +14,43 @@ package com.jboss.jbds.integration_stack.tests.test;
 import java.util.HashSet;
 import java.util.Set;
 
+/**
+ * @author pleacu
+ *
+ */
+/**
+ * @author pleacu
+ *
+ */
 public interface IDEPropertiesConstants {
 
-   final static String PROPERTIES_LOCATION = "http://download.jboss.org/jbosstools/configuration/ide-config.properties";
+   /** The property file containing JBT configuration URLs. */
+   String PROPERTIES_LOCATION =
+     "http://download.jboss.org/jbosstools/configuration/ide-config.properties";
 
+   /** Set of known Integration Stack URL properties. */
    @SuppressWarnings("serial")
-   static final Set<String> knownProperties = new HashSet<String>() {
+   Set<String> KNOWN_PROPERTIES = new HashSet<String>() {
       {
          add("jboss.discovery.site.integration-stack.url");
          add("jboss.discovery.earlyaccess.site.integration-stack.url");
          add("jboss.discovery.site.integration-stack-sap.url");
       }
    };
-	
+
+   /** Set of Integration Stack targets - devstudio = production,
+    * jbosstools = community. */
    @SuppressWarnings("serial")
-   static final Set<String> knownTargets = new HashSet<String>() {
+   Set<String> KNOWN_TARGETS = new HashSet<String>() {
       {
          add("devstudio");
          add("jbosstools");
       }
    };
 
+   /** Set of Integration Stack build version qualifiers (not complete). */
    @SuppressWarnings("serial")
-   static final Set<String> knownVersionQualifiers = new HashSet<String>() {
+   Set<String> KNOWN_VERSION_QUALIFIERS = new HashSet<String>() {
       {
          add("Alpha1");
          add("Alpha2");
@@ -49,5 +63,5 @@ public interface IDEPropertiesConstants {
          add("GA");
       }
    };
-	
+
 }
